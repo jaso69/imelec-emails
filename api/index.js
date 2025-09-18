9157b95333a1c61fe04f810c6ec6f3ab1b5c3edf
jaso69 <joseonieva@gmail.com>
2025-09-18 19:09:44 +0200

    error 3

--- api/index.js
+++ api/index.js
@@ -43,6 +43,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ success: true, data });
   } catch (err) {
-    return res.status(500).json({ error: err.message || 'Error interno del servidor' });
+    return res.status(500).json({ error: 'error interno' || 'Error interno del servidor' });
   }
 }
